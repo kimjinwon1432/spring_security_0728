f88b97ddfdd53175e10ffae9343a40978b4c0fcf
kimjinwon1432 <giantstar115@naver.com>
2024-07-29 10:05:25 +0900

    Spring Security 설정

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spring_security_0728
+
+1. security 추가
+2. main page생성하여 로그인 후 페이지 이동되는지 확인
+3. spring config 클래스 등록 및 필터 개념
--- TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ TestSecurity/src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -4,13 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    
+    // 패스워드 암호화 메소드
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        // 특정 경로에 대한 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/login", "/join").permitAll()
@@ -19,6 +29,17 @@ public class SecurityConfig {
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 );
+        // form 로그인
+        http
+                .formLogin((auth) -> auth.loginPage("/login") // 권한이 없을 시 리다이렉트 시킴
+                        .loginProcessingUrl("/loginProc") // 이 겅로로 로그인처리 진행
+                        .permitAll()
+                );
+
+        // csrf 비활성화
+        http
+                .csrf((auth) -> auth.disable());
+
         return http.build();
     }
 }
