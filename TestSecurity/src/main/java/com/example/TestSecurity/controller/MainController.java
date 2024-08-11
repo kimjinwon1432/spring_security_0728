b88d591fda2dd1c14ff29dc597037ff8b328a9f3
kimjinwon1432 <giantstar115@naver.com>
2024-08-11 14:27:33 +0900

    1. UserEntity UserDetails 상속    authorities 삽입 2. MainController.java   - model에 id 삽입

--- TestSecurity/src/main/java/com/example/TestSecurity/controller/MainController.java
+++ TestSecurity/src/main/java/com/example/TestSecurity/controller/MainController.java
@@ -1,12 +1,18 @@
 package com.example.TestSecurity.controller;
 
+import com.example.TestSecurity.entity.UserEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String main(){
+    public String main(Model model, Authentication auth){
+
+        UserEntity entity = (UserEntity) auth.getPrincipal();
+        model.addAttribute(entity.getId());
         return "main";
     }
 }
--- TestSecurity/src/main/java/com/example/TestSecurity/entity/UserEntity.java
+++ TestSecurity/src/main/java/com/example/TestSecurity/entity/UserEntity.java
@@ -6,12 +6,18 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.*;
+
+import java.util.Collection;
 
 // user의 entity임을 알리는 어노테이션.. 유저 정보를 담을 바구니라고 생각하면됨.
 @Entity
 @Setter
 @Getter
-public class UserEntity {
+public class UserEntity implements UserDetails {
     // entitu는 id값이 필수적으로 필요
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,4 +29,17 @@ public class UserEntity {
 
     // 권한 저장을 위한 role
     private String role;
+    /**
+     * 계정 권한(Spring security Role)
+     */
+    private Set<GrantedAuthority> authorities;
+    /* Getter/Setter */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+//        this.authorities = Collections.unmodifiableSet(sortAuthorities(authorities));
+    }
 }
